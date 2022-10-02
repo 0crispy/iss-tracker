a83a4eb3e702d6229ef4a8344ce1f967a49f67b4
Cwispy <krispastas@gmail.com>
2022-10-02 12:48:54 +0300

    Fixed daylight cycle

--- project/scripts/Sun.gd
+++ project/scripts/Sun.gd
@@ -6,5 +6,6 @@ onready var sun_handle = get_node(sun_handle_path)
 func _process(delta):
 	var datetime_dict = Time.get_datetime_dict_from_system(true )
 	var hours = float(datetime_dict.hour) + float(datetime_dict.minute)/60.0
-	rotation_degrees.y = hours/24.0*360.0 + 180.0
-	sun_handle.rotation_degrees.y = hours/24.0*360.0 + 180.0
+	rotation_degrees.y = -hours/24.0*360.0
+	sun_handle.rotation_degrees.y = -hours/24.0*360.0
+	print()
